c6d7bd65758fff521657ec22d0f3ca56724a1f34
Benitofarina03 <farinasaverio03@gmail.com>
2024-11-23 11:15:54 +0100

    Conferma codice a barre
    Componente che permette di inserire il codice a barre e confermarlo, sarà una modale nella pagina dove verrà aggiunto

---
+++ Development/ZWH-FrontEnd/src/app/components/conferma-codice-a-barre/conferma-codice-a-barre.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { navigationBtnComponent } from '../navigationBtn/navigationBtn.component';
+
+@Component({
+  selector: 'app-conferma-codice-a-barre',
+  standalone: true,
+  imports: [
+    navigationBtnComponent,
+  ],
+  templateUrl: './conferma-codice-a-barre.component.html',
+  styleUrl: './conferma-codice-a-barre.component.css'
+})
+export class ConfermaCodiceABarreComponent {
+
+}
