2f88c5f5544b6b50070182d3a681cab3ec84ad81
MarcoMeg03 <168180151+MarcoMeg03@users.noreply.github.com>
2024-11-24 09:22:28 +0100

    aggiunta popup semplice e popup con form segnalazione
    Aggiunta elemento popup-semplice: si tratta di un elemento popup che se inserito appare invisibile (display: none) e può essere mostrato eseguendo la funzione showPopup("testo.."); mostrerà il testo che gli viene passato.. l'ho diviso come componente perchè in alcuni casi c'è bisogno di un popup che non sia collegato ad un bottone navigazionale o utente, ma per esempio ad una voce del menu.
    
    Aggiunta elemento bottone-popup-integrato: si tratta di un pulsante pronto all'uso , se inserito nella pagina richiamerà automanticamente popup-semplice,
    accetta come input un paramentro "messaggio" che sarà il messaggio che sarà visualizzato a schermo tramita il popup.
    
    aggiunta bottone-form-segnalazione-integrato : simile al precedente , ma invece che testo visualizza un piccolo form, e non possiede un attributo "messaggio"

---
+++ Development/ZWH-FrontEnd/src/app/components/popup-semplice/popup-semplice.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
+
+@Component({
+  selector: 'app-popup-semplice',
+  standalone: true,
+  templateUrl: './popup-semplice.component.html',
+  styleUrls: ['./popup-semplice.component.css'],
+  imports: [CommonModule],
+})
+export class PopupSempliceComponent {
+  visible: boolean = false;  // Determina se il popup è visibile
+  message: string = '';  // Messaggio da visualizzare nel popup
+
+  // Metodo per aprire il popup
+  openPopup(message: string) {
+    this.message = message; // Imposta il messaggio
+    this.visible = true; // Mostra il popup
+  }
+
+  // Metodo per chiudere il popup
+  closePopup() {
+    this.visible = false; // Nasconde il popup
+  }
+}
