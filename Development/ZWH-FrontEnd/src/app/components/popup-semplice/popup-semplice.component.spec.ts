faf654d32a429d69447bbc8c04f9550e5c56a115
MarcoMeg03 <168180151+MarcoMeg03@users.noreply.github.com>
2024-11-25 13:19:04 +0100

    Aggiunta popup-semplice e bottone-popup-integrato
    popup-semplice è un componente che permette di visualizzare a schermo un popup in overlay che presenta un testo specificato tramite il parametro "messaggio".
    
    bottone-popup-integrato è un componente che permette l'inserimento di un bottone con popup-semplice integrato, premendo il pulsante appare direttamente il popup-semplice.

---
+++ Development/ZWH-FrontEnd/src/app/components/popup-semplice/popup-semplice.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PopupSempliceComponent } from './popup-semplice.component';
+
+describe('PopupSempliceComponent', () => {
+  let component: PopupSempliceComponent;
+  let fixture: ComponentFixture<PopupSempliceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PopupSempliceComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PopupSempliceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
