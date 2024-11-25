8319c3ba3a21adae4639d77659d1909a8c0b5eff
MarcoMeg03 <168180151+MarcoMeg03@users.noreply.github.com>
2024-11-25 13:21:21 +0100

    Aggiunta bottone-popup-segnalazione-integrato
    Permette di inserire un pulsante con un popup in overlay integrato , che se cliccato mostra un form all'interno di un popup per inviare una segnalazione.

---
+++ Development/ZWH-FrontEnd/src/app/components/bottone-popup-segnalazione-integrato/bottone-popup-segnalazione-integrato.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PopupFormSegnalazioneComponent } from './bottone-popup-segnalazione-integrato.component';
+
+describe('BottonePopupSegnalazioneIntegrato', () => {
+  let component: PopupFormSegnalazioneComponent;
+  let fixture: ComponentFixture<PopupFormSegnalazioneComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PopupFormSegnalazioneComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PopupFormSegnalazioneComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
