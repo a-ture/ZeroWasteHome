360ed4769cc72feaf85172b7cafd0069b3b9da2e
MarcoMeg03 <168180151+MarcoMeg03@users.noreply.github.com>
2024-11-26 21:07:47 +0100

    Inserimento pulsante e form per inviare una segnalazione
    Integrazione del modale per la richiesta di assistenza
    - Configurato il pulsante "Spiegaci il tuo problema" per innescare l'esecuzione del metodo openModal(), che utilizza il nuovo servizio introdotto `FormSegnalazioneModalService` per rendere visibile il modale.

---
+++ Development/ZWH-FrontEnd/src/app/components/bottone-form-segnalazione/bottone-form-segnalazione.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BottoneFormSegnalazioneComponent } from './bottone-form-segnalazione.component';
+
+describe('BottoneFormSegnalazioneComponent', () => {
+  let component: BottoneFormSegnalazioneComponent;
+  let fixture: ComponentFixture<BottoneFormSegnalazioneComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BottoneFormSegnalazioneComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BottoneFormSegnalazioneComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
