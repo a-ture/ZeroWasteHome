69d9324c4ba5737139947c98222ca39edd2be065
Benitofarina03 <farinasaverio03@gmail.com>
2024-12-14 23:49:53 +0100

    Aggiunta pagina Gestore Pagamento
    Aggiunti i componenti alla pagina per il gestore dei pagamenti

---
+++ Development/ZWH-FrontEnd/src/app/pages/gestore-report-pagamento/gestore-report-pagamento.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import {
+  GestoreReportPaymentTableComponent
+} from '../../components/gestore-report-payment-table/gestore-report-payment-table.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-gestore-report-pagamento',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    GestoreReportPaymentTableComponent,
+    FooterComponent,
+  ],
+  templateUrl: './gestore-report-pagamento.component.html',
+  styleUrl: './gestore-report-pagamento.component.css'
+})
+export class GestoreReportPagamentoComponent {
+  reports = [
+    {
+      info: [
+        {
+          descrizione: 'Segnalazione 1',
+          utente: 'Mario Rossi',
+          dataCreazione: new Date('2024-01-01'),
+          statoSegnalazione: 'APERTA',
+          dettagli: 'Questi sono i dettagli della segnalazione 1',
+        },
+      ],
+    },
+    {
+      info: [
+        {
+          descrizione: 'Segnalazione 2',
+          utente: 'Luigi Verdi',
+          dataCreazione: new Date('2024-01-02'),
+          statoSegnalazione: 'APERTA',
+          dettagli: 'Questi sono i dettagli della segnalazione 2',
+        },
+      ],
+    },
+    {
+      info: [
+        {
+          descrizione: 'Segnalazione 3',
+          utente: 'Giulia Bianchi',
+          dataCreazione: new Date('2024-01-03'),
+          statoSegnalazione: 'IN_RISOLUZIONE',
+          dettagli: 'Questi sono i dettagli della segnalazione 3',
+        },
+      ],
+    },
+  ];
+}
